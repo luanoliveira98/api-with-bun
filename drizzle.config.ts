2d14b7cace8f89ac4d22be005f0df28812294050
Luan Oliveira <luanoliveira17@hotmail.com>
2024-12-23 15:53:39 -0300

    build: run migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'drizzle-kit'
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema/index.ts',
-  out: "./drizzle"
+  out: "./drizzle",
+  dbCredentials: {
+    url: 'postgresql://docker:docker@localhost:5432/pizza_shop'
+  }
 })
