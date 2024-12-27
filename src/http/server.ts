92edd0459025af0ec603fce7089053eeab66bd5e
Luan Oliveira <luanoliveira17@hotmail.com>
2024-12-27 15:33:50 -0300

    refactor: add error handling

--- src/http/server.ts
+++ src/http/server.ts
@@ -13,6 +13,19 @@ const app = new Elysia()
   .use(signOutRoute)
   .use(getProfileRoute)
   .use(getManagedRestarauntRoute)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'VALIDATION': {
+        set.status = error.status
+        return error.toResponse()
+      }
+      default: {
+        console.log(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
 
 app.listen(3333, () => {
   console.log('HTTP server running!')
